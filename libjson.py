883ceae93b2a77139e4e94c30027adffa2d66373
Marko Kevac <marko@kevac.org>
2011-04-13 17:38:08 +0400

    added libjson
    Signed-off-by: Marko Kevac <marko@kevac.org>

---
+++ libjson.py
@@ -0,0 +1,25 @@
+from waflib.Configure import conf
+from waflib import Task
+from waflib.TaskGen import extension
+
+def options(opt):
+    opt.add_option('--libjson', action='store', default='/usr',
+            dest='libjson')
+
+@conf
+def check_libjson(self):
+    self.start_msg("Checking for the variable LIBJSON")
+
+    if self.options.libjson:
+        self.env.LIBJSON = self.options.libjson
+        self.env.append_unique("CFLAGS_LIBJSON", "-I" + self.env.LIBJSON + '/include')
+        self.env.append_unique("LIBPATH_LIBJSON", self.env.LIBJSON + '/lib')
+        self.end_msg(self.env.LIBJSON)
+    else:
+        self.end_msg("LIBJSON is not set")
+
+    return True
+
+def configure(conf):
+    conf.check_libjson(mandatory=True)
+
